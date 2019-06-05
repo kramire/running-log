7156156dfdaffd815c06b28de7f4ee46662e00f8
kramire <kramire@users.noreply.github.com>
2019-06-05 17:03:05 +0200

    (style) AddRun styled with Bulma modal. (feat) can click Add Run button to toggle modal.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+import '../node_modules/bulma/css/bulma.css';
 
 import { AddRun } from './components';
 
+export const AppContext = React.createContext();
+
 function App() {
+
+  const [isModalActive, setModal] = useState('');
+
+  const toggleAddRun = () => isModalActive === '' ? setModal('is-active') : setModal('');
+
   return (
-    <div className="App">Hi there
-      <AddRun></AddRun>
-    </div>
+    <AppContext.Provider value={{isModalActive, toggleAddRun}}>
+      <div className="App">Hi there
+        <button onClick={toggleAddRun}>Add Run +</button>
+        <AddRun></AddRun>
+      </div>
+    </AppContext.Provider>
   );
 }
 
--- client/src/components/AddRun/AddRun.js
+++ client/src/components/AddRun/AddRun.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import '../../../node_modules/bulma/css/bulma.css';
 // import moment from '../../../../moment';
 // need to build event to handle form submission
-// vx
+import { AppContext } from '../../App';
 
 
-function AddRun(props) {
+function AddRun() {
+
+  const { isModalActive, toggleAddRun } = useContext(AppContext);
+  console.log(isModalActive);
+  console.log(toggleAddRun);
 
   const [distance, setDistance] = useState('');
   const [date, setDate] = useState(Date.now());
@@ -29,56 +34,73 @@ function AddRun(props) {
   }
 
   return (
-    <div>
-      <h1>Log New Run</h1>
-      <form>
-        <label>
-        Distance 
-          <input type='text' value={distance} required
-          onChange={(e) => setDistance(e.target.value)}></input>
-        Miles
-        </label>
-        <label>
-        Date 
-          <input type='date' value={date} required
-          onChange={(e) => setDate(e.target.value)}></input>
-        </label>
-        <label>
-        Location 
-          <input type='text' value={location}
-          onChange={(e) => setLocation(e.target.value)}></input>
-        </label>
-        <label>
-        Notes 
-          <textarea value={note}
-          onChange={(e) => setNote(e.target.value)}/>
-        </label>
-        <label>
-        Type of Run 
-          <input type='button' value='Speed'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Distance'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Tempo'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Easy'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Intervals'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Hills'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Recovery'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Fartlek'
-          onClick={(e) => runTypeEvent(e)}/>
-          <input type='button' value='Progression'
-          onClick={(e) => runTypeEvent(e)}/>
-        </label>
-        <label>
-          <input type='submit' value='Save Run'
-          onSubmit={(e) => saveForm(e)}></input>
-        </label>
-      </form>
+    <div className={`modal has-background-white-ter ${isModalActive}`}>
+    <div className='modal-background'></div>
+      <div className='modal-card'>
+        <h1 className='modal-card-head has-text-centered modal-card-title'>Log New Run</h1>
+        <form className='modal-card-body'>
+        <div className='field is-horizontal'>
+            <label className='label field-label'>Distance</label>
+              <div className='control field-body'>
+                <input className='input' type='text' value={distance} required
+                onChange={(e) => setDistance(e.target.value)}></input>
+                <span>Miles</span>
+              </div>
+          </div>
+          <div className='field is-horizontal'>
+            <label className='label field-label'>Date</label>
+              <div className='control field-body'>
+                <input className='input' type='date' value={date} required
+                onChange={(e) => setDate(e.target.value)}></input>
+              </div>
+          </div>
+          <div className='field is-horizontal'>
+            <label className='label field-label'>Location</label>
+              <div className='control field-body'>
+                <input className='input' type='text' value={location} placeholder='e.g. "New York, NY" or "10021"'
+                onChange={(e) => setLocation(e.target.value)}></input>
+              </div>
+          </div>
+          <div className='field is-horizontal'>
+            <label className='label field-label'>Notes</label>
+              <div className='control field-body'>
+                <textarea className='textarea' value={note} placeholder="Anything to note?"
+                onChange={(e) => setNote(e.target.value)}/>
+              </div>
+          </div>
+          <div className='field is-horizontal'>
+            <label className='label field-label'>Type of Run</label>
+              <div className='control field-body buttons'>
+                <input className='button' type='button' value='Speed'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Distance'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Tempo'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Easy'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Intervals'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Hills'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Recovery'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Fartlek'
+                onClick={(e) => runTypeEvent(e)}/>
+                <input className='button' type='button' value='Progression'
+                onClick={(e) => runTypeEvent(e)}/>
+              </div>
+          </div>
+          <div className='field'>
+            <div className='control has-text-centered'>
+                <input className='button is-success' type='submit' value='Save Run'
+                onSubmit={(e) => saveForm(e)}></input>
+            </div>
+          </div>
+        </form>
+      </div>
+    <button className="modal-close is-large" aria-label="close"
+    onClick={toggleAddRun}></button>
     </div>
   )
 }
